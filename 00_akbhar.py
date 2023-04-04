37f0eee8daea9e5ff0305d44b356fe8ef75011b5
Shadow0992004 <109844714+Shadow0992004@users.noreply.github.com>
2023-04-04 15:04:29 +0530

    Add files via upload

---
+++ 00_akbhar.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def speak(str):
+    from win32com.client import Dispatch
+    
+    speak=Dispatch("SAPI.Spvoice")
+    speak.speak(str)
+
+if __name__=="__main__":
+    speak("News for today..lets begin ")
+    API="3700f11302c74b7e92b431dbdfc67fd8"
+    url=f"https://newsapi.org/v2/top-headlines?country=in&apiKey={API}"
+    news=requests.get(url).text
+    news_dict=json.loads(news)
+    # print(news_dict["articles"])
+    arts=news_dict["articles"]
+    for articles in arts:
+        speak(articles['title'])
+        speak("Moving on to the next news!! Listen carefully")
+    print("thanks for listening.....")
