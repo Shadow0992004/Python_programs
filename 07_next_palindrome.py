997c411f95deaee50090e5045449383e2a024af3
Shadow0992004 <109844714+Shadow0992004@users.noreply.github.com>
2023-04-30 11:30:45 +0530

    Create 07_next_palindrome.py

---
+++ 07_next_palindrome.py
@@ -0,0 +1,20 @@
+def is_palindrome(s):
+    a=str(s)
+    return a==a[::-1]                       
+
+def next_palindrome(n):
+    n=n+1
+    while not is_palindrome(n):
+        n=n+1
+    return n
+
+if __name__=="__main__":
+    num=int(input("enter number of test subjects: \n"))
+    list=[]
+    for i in range(num):
+        a=int(input(f"enter element no \"{i+1}\" to check its next PALINDROME: \n"))
+        list.append(a)
+
+    for i in range(num):
+        print(f"Next PALINDROME for {list[i]} is {next_palindrome(list[i])}")
+
